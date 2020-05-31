d7963309000a43ccacb3051bb2b3f0df45dcc8ec
oikumo <oikumo@gmail.com>
2020-05-30 23:05:20 -0400

    Refactor Rect

--- index.mjs
+++ index.mjs
@@ -3,7 +3,8 @@ import { RenderTexture } from './engine/html/render-texture.mjs'
 import { Entity } from './engine/world/entity.mjs'
 import { Texture } from './engine/rendering/textures/texture.mjs'
 import { moveTo } from './engine/math/vector.mjs'
-import { rectPointsInOther, Rect } from './engine/math/geometry.mjs'
+import { createRectFromCenter, createRectFromCorners, inRect } from './engine/math/rect.mjs'
+import { rectInOther } from './engine/math/rect-collisions.mjs'
 import { uiComponent } from './engine/ui/ui-component.mjs'
 
 export {
@@ -12,7 +13,9 @@ export {
     Entity,
     Texture,
     moveTo,
-    rectPointsInOther,
-    Rect,
-    uiComponent
+    uiComponent,
+    createRectFromCenter,
+    createRectFromCorners,
+    inRect,
+    rectInOther
 }
