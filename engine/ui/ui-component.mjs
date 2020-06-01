9757495222d2bd52805b1842d5375731b45a5d83
oikumo <oikumo@gmail.com>
2020-05-31 20:30:00 -0400

    Fixed ui component drawer

--- engine/ui/ui-component.mjs
+++ engine/ui/ui-component.mjs
@@ -8,16 +8,20 @@ const createUiComponent = (rect, backgroundColor) => {
 }
 
 const drawUiComponent = (component, target, targetWidth) => {
-    const width = component.width
-    const size = component.width * component.height
-    const topX = component.rect.topLeft.x
-    const topY = component.rect.topLeft.y
     const color = component.backgroundColor
+    const rect = component.rect
+    const width = rect.width
+    const size = rect.width * rect.height
+
+    const dx = rect.bottomLeft.x
+    const dy = rect.bottomLeft.y
+
     let col = 0
     let row = 0
+
     for (let i = 0; i < size; i++) {
-        target[Math.ceil(topX + col) + Math.ceil(topY + row) * targetWidth] = color()
-        if (col + 1 == width) {
+        target[(dx + col) + (dy + row) * targetWidth] = color()
+        if (col + 1 === width) {
             col = 0
             row++
         }
--- tests/engine/ui/ui-component-test.mjs
+++ tests/engine/ui/ui-component-test.mjs
@@ -1,33 +1,63 @@
-import { equals, test } from 'naive-tests'
+import { equals, objAreEquals, test } from 'naive-tests'
 import { ui, createRectFromCorners } from '../../../index.mjs'
 const { createUiComponent, drawUiComponent } = ui
 
+test('create ui component', () => {
+    const backgroundColor = 0xFF555555
+    const rect = createRectFromCorners({ x: 45, y: 65 }, { x: 55, y: 35 })
+    const component = createUiComponent(rect, backgroundColor)
+    equals(component.backgroundColor(), backgroundColor)
+    objAreEquals(component.rect, {
+        center: { x: 50, y: 50 },
+        width: 10,
+        height: 30,
+        topLeft: { x: 45, y: 65 },
+        topRight: { x: 55, y: 65 },
+        bottomLeft: { x: 45, y: 35 },
+        bottomRight: { x: 55, y: 35 },
+    })
+})
+
+const inComponent = (col, row, component) => {
+    const xMin = component.rect.topLeft.x
+    const xMax = component.rect.topRight.x
+
+    const yMin = component.rect.bottomRight.y
+    const yMax = component.rect.topLeft.y
+
+    return xMin <= col && col < xMax
+        && yMin <= row && row < yMax
+}
+
 test('draw ui component', () => {
-    const rect = createRectFromCorners({ x: 5, y: 5 }, { x: 5, y: 5 })
+    const rect = createRectFromCorners({ x: 2, y: 5 }, { x: 4, y: 1 })
+    equals(rect.width, 2)
+    equals(rect.height, 4)
     const backgroundColor = 0xFF555555
     const component = createUiComponent(rect, backgroundColor)
 
-    const targetTextureWidth = 10
-    const targetTextureHeight = 20
-    const targetTexture = new Uint32Array(targetTextureWidth * targetTextureHeight)
+    const pixelsWidth = 6
+    const pixelsHeight = 7
+    const pixelsSize = pixelsWidth * pixelsHeight
+    const pixles = new Uint32Array(pixelsSize)
 
-    drawUiComponent(component, targetTexture, targetTextureWidth)
+    drawUiComponent(component, pixles, pixelsWidth)
 
-    const componentTop = component.rect.topLeft
-    const componentWidth = component.rect.width
-    const componentHight = component.rect.height
-    const targetTextureSize = targetTextureWidth * targetTextureHeight
+    let paintedPixels = 0
+    const componenentSize = rect.width * rect.height
+
     let col = 0
     let row = 0
-    for (let i = 0; i < targetTextureSize; i++) {
-        if (componentTop.x <= col && col < componentTop.x + componentWidth
-            && componentTop.y <= row && row < componentTop.y + componentHight) {
-            equals(targetTexture[i], backgroundColor)
+    for (let i = 0; i < pixelsSize; i++) {
+        if (inComponent(col, row, component)) {
+            equals(pixles[i], backgroundColor)
+            paintedPixels++
         }
         else {
-            equals(targetTexture[i], 0)
+            equals(pixles[i], 0)
         }
-        if (col + 1 == targetTextureWidth) {
+
+        if (col + 1 == pixelsWidth) {
             col = 0
             row++
         }
@@ -35,4 +65,6 @@ test('draw ui component', () => {
             col++
         }
     }
+
+    equals(paintedPixels, componenentSize)
 })
