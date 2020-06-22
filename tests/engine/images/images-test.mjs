fe6032d17f1d22192f7f590fe439018a26f6ebd5
oikumo <oikumo@gmail.com>
2020-06-21 23:48:32 -0400

    Fix

--- tests/engine/images/images-test.mjs
+++ tests/engine/images/images-test.mjs
@@ -4,11 +4,9 @@ import { Texture } from '../../../engine/rendering/textures/texture.mjs'
 
 const { notEquals } = assertions
 
-test('texture2bmp', () => {
-    const texture = new Texture(2, 4)
+test('texture2bmp big texture', () => {
+    const texture = new Texture(10000, 10000)
     texture.fill(() => parseInt(0xFF0000FF))
-    texture.pixels[0] = 0xFFFF0000
-    texture.pixels[3] = 0xFF00FF00
     const bmp = texture2bmp(texture)
     notEquals(null, bmp)
     notEquals(undefined, bmp)
