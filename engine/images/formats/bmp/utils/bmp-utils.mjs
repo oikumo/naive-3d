0480cf7f6662fd307b041b70fea7f375669147f8
oikumo <oikumo@gmail.com>
2020-06-21 23:43:32 -0400

    Fix

--- engine/images/formats/bmp/utils/bmp-utils.mjs
+++ engine/images/formats/bmp/utils/bmp-utils.mjs
@@ -3,7 +3,7 @@ const getPixelsBytesPerRow = (width, bytesPerPixel) => width * bytesPerPixel
 const getPaddingBytesPerRow = (width, bytesPerPixel) => {
     const pixelsBytesPerRow = getPixelsBytesPerRow(width, bytesPerPixel)
     const module = pixelsBytesPerRow % 4
-    const paddingBytesPerRow = module === 0 ? 4 - width : 4 - module
+    const paddingBytesPerRow = module === 0 ? 0 : 4 - module
     return paddingBytesPerRow
 }
 
