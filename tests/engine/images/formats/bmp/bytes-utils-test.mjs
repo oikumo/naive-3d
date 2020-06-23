2a2d2cc49fcfbe586071fc25d1b2ed8fed760fc8
oikumo <oikumo@gmail.com>
2020-06-22 22:08:07 -0400

    Refactor

--- tests/engine/images/formats/bmp/bytes-utils-test.mjs
+++ tests/engine/images/formats/bmp/bytes-utils-test.mjs
@@ -40,7 +40,7 @@ test('convert abgr to bgr color', () => {
 test('bmp converter - hex to binary', () => {
     const imageHex = '424D460000000000000036000000280000000200000002000000010018000000000010000000130B0000130B000000000000000000000000FFFFFFFF0000FF000000FF000000'
     const convertedBin = hex2bin(imageHex)
-    const binFilePath = path.join(fileURLToPath(import.meta.url), '../../../../../../', 'tests-resources/engine/images/formats/bmp/file-image-bin')
+    const binFilePath = path.join(fileURLToPath(import.meta.url), '../../../../../../', 'tests-resources/engine/images/formats/bmp/file-image-bin.bmp')
     const binFile = fs.readFileSync(binFilePath)
     sameArrayElementsOnly(binFile, convertedBin)
 })
