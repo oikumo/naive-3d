a3de2fc7ec988915577c68f6dd7cb26fc9fbbfd5
oikumo <oikumo@gmail.com>
2021-04-10 18:23:36 -0400

    WIP Refactor

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export { Image } from './src/core/images/image.js';
 export { Color } from './src/core/colors/color.js';
 export { Textures } from './src/core/textures/index.js';
 export * from './src/core/ui/index.js';
--- tests/core/images/images-test.js
+++ tests/core/images/images-test.js
@@ -2,14 +2,14 @@ import { test, assert } from 'naive-tests';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { texture2bmp, bmp2texture } from '../../../src/core/images/images.js';
 import { Textures } from '../../../index.js';
 import { Color } from '../../../index.js';
+import { Image } from '../../../src/core/images/image.js';
 
 test('texture2bmp big texture', () => {
     const texture = Textures.createTexture(1000, 1000);
     texture.fill(() => parseInt(0xFF0000FF));
-    const bmp = texture2bmp(texture);
+    const bmp = Image.texture2bmp(texture);
     assert.notEquals(null, bmp);
     assert.notEquals(undefined, bmp);
 });
@@ -17,7 +17,7 @@ test('texture2bmp big texture', () => {
 test('bmp2texture', () => {
     const binFilePath = path.join(fileURLToPath(import.meta.url), '../../../../', 'tests-resources/engine/images/formats/bmp/file-image-bin.bmp');
     const binFile = fs.readFileSync(binFilePath);
-    const texture = bmp2texture(binFile);
+    const texture = Image.bmp2texture(binFile);
     const width = 2;
     const height = 2;
     assert.equals(width, texture.width);
