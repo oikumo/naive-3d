ba01e8e684b7c0df3ba5d28b5ee32d9a6e99f0eb
oikumo <oikumo@gmail.com>
2021-04-11 20:57:56 -0400

    Changed version; Refactor

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export * from './src/core/geometry/geometry.js';
 export { Image } from './src/core/images/image.js';
 export { Color } from './src/core/colors/color.js';
 export { Textures } from './src/core/textures/textures.js';
